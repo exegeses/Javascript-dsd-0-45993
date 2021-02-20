6976a2e7b79a5cddbb9e31c70225341e832aeb16
Marcos Pinardi <exegeses@gmail.com>
2021-02-20 10:47:20 -0300

    repaso de las clases 1 y 2

---
+++ clase3/js/repaso.js
@@ -0,0 +1,9 @@
+//imprimimos en el documento
+    document.write('Hola mundo desde javascript');
+    document.write('<br>');
+// pedimos que el usuario ingrese un texto
+   var nombre = prompt('ingrese un texto');
+
+// imprimimos dato de la variable en el documento
+
+    document.write( 'Bienvenido ' + nombre );
