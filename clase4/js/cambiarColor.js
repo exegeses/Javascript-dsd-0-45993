55115cc33f1a6e8e9292dca2642a48321358b828
Marcos Pinardi <exegeses@gmail.com>
2021-02-27 10:58:55 -0300

    cambiar color usando funciones

---
+++ clase4/js/cambiarColor.js
@@ -0,0 +1,14 @@
+    // ubicamos elemento en HMTL
+    var caja = document.getElementById('caja');
+
+    function verde()
+    {
+        caja.style.backgroundColor = '#a3d6c3';
+        caja.style.border = '1px solid #425650';
+    }
+
+    function naranja()
+    {
+        caja.style.backgroundColor = '#d6bfa3';
+        caja.style.border = '1px solid #f60';
+    }
